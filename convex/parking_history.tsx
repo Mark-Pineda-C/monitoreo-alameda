6ffd58a6b1f08a9f1aed14168eb8a0c639c64845
Mark Pineda <mfpc1500@gmail.com>
2025-02-02 09:54:13 -0500

    live-test: add function to add circular dep in parking_lots and clients

--- convex/parking_history.tsx
+++ convex/parking_history.tsx
@@ -1,8 +1,34 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const getRecentLogs = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("parking_history").withIndex("by_creation_time").order("desc").take(3);
+  },
+});
+
+export const test = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const clients = await ctx.db.query("clients").collect();
+    for (const client of clients) {
+      if (typeof client.parking_lot === "string") {
+        const lot = await ctx.db.get(client.parking_lot);
+        if (lot) {
+          await ctx.db.patch(lot._id, {
+            defined_client: client._id,
+          });
+        }
+      } else {
+        const lots = await Promise.all(client.parking_lot.map((lot) => ctx.db.get(lot)));
+        for (const lot of lots) {
+          if (lot) {
+            await ctx.db.patch(lot._id, {
+              defined_client: client._id,
+            });
+          }
+        }
+      }
+    }
   },
 });
